a1bf4473af3c79440a33df279b4647beac91d902
Marcelo <marcelo@siasp.com.br>
2024-02-08 16:50:51 -0300

    Versao 1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,79 @@
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./components/NewNoteCard";
 import { NoteCard } from "./components/NoteCard";
 
+interface NoteProps {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [notes, setNotes] = useState<NoteProps[]>(() => {
+    const notesOnStorage = localStorage.getItem("notes");
+
+    if (notesOnStorage) return JSON.parse(notesOnStorage);
+
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function onNoteDelete(id: string) {
+    const notesArray = notes.filter((note) => note.id !== id);
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const query = e.target.value;
+
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLowerCase().includes(search.toLowerCase())
+        )
+      : notes;
+
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-4 sm:mx-20">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="Expert Notes" />
       <form className="w-full">
         <input
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard
-          note={{
-            date: new Date(2024, 1, 1),
-            content: "Card",
-          }}
-        />
-        <NoteCard
-          note={{
-            date: new Date(2022, 1, 1),
-            content: "Card",
-          }}
-        />
-        <NoteCard
-          note={{
-            date: new Date(2023, 1, 1),
-            content: "Card",
-          }}
-        />
+      <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map((item: any) => (
+          <NoteCard note={item} onNoteDeleted={onNoteDelete} />
+        ))}
       </div>
     </div>
   );
--- src/components/NoteCard/index.tsx
+++ src/components/NoteCard/index.tsx
@@ -3,14 +3,16 @@ import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { X } from "lucide-react";
 
-export interface NoteCardProps {
+interface NoteCardProps {
   note: {
+    id: string;
     date: Date;
     content: string;
   };
+  onNoteDeleted: (id: string) => void;
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard(props: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger
@@ -18,32 +20,35 @@ export function NoteCard({ note }: NoteCardProps) {
     "
       >
         <span className="text-sm font-medium text-slate-300">
-          {formatDistanceToNow(note.date, {
+          {formatDistanceToNow(props.note.date, {
             locale: ptBR,
             addSuffix: true,
           })}
         </span>
-        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <p className="text-sm leading-6 text-slate-400">{props.note.content}</p>
 
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/5 pointer-events-none" />
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="overflow-hidden fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full h-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
-              {formatDistanceToNow(note.date, {
+              {formatDistanceToNow(props.note.date, {
                 locale: ptBR,
                 addSuffix: true,
               })}
             </span>
-            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+            <p className="text-sm leading-6 text-slate-400">
+              {props.note.content}
+            </p>
           </div>
           <button
             type="button"
+            onClick={() => props.onNoteDeleted(props.note.id)}
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
             Deseja{" "}
